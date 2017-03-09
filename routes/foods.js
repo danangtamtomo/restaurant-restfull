1b88f7298261240115003a01628d66888c943f9b
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-03-09 11:09:08 +0700

    Add Routes

--- routes/foods.js
+++ routes/foods.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const FoodController = require('../controllers/FoodController')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', FoodController.getFoods)
+router.get('/', FoodController.getFood)
+router.post('/', FoodController.createFood)
+router.put('/', FoodController.updateFood)
+router.delete('/', FoodController.deleteFood)
 
-module.exports = router;
+module.exports = router
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const RestaurantController = require('../controllers/'RestaurantController)
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', RestaurantController.getRestaurants)
+router.get('/', RestaurantController.getRestaurant)
+router.post('/', RestaurantController.createRestaurant)
+router.put('/', RestaurantController.updateRestaurant)
+router.delete('/', RestaurantController.deleteRestaurant)
 
-module.exports = router;
+module.exports = router
